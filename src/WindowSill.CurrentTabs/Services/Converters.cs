14687a79b8450daf6681cca43fae509a4e3f393b
DZimo <53861899+DZimo@users.noreply.github.com>
2025-12-18 10:41:04 +0100

    Created new current tabs project

--- src/WindowSill.CurrentTabs/Services/Converters.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WindowSill.SimpleCalculator.Services
-{
-    public static class Converters
-    {
-        public static readonly IValueConverter ArithmeticOpConverter = new ArithmeticOpConverter();
-    }
-}
