51698b2be4b80d2ddc81ed41e74d1de61aab3038
DZimo <53861899+DZimo@users.noreply.github.com>
2026-01-27 20:27:28 +0100

    add of screen recorder proj

---
+++ src/WindowSill.ScreenRecorder/dev/SmartCalculator/SmartCalculatorActivator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Recognizers.Text;
+using NotepadBasedCalculator.Api;
+using System.ComponentModel.Composition;
+using WindowSill.API;
+using WindowSill.SimpleCalculator.UI;
+using WindowSill.SimpleCalculator.ViewModels;
+
+namespace WindowSill.SimpleCalculator.SmartCalculator
+{
+    [Export(typeof(ISillTextSelectionActivator))]
+    [ActivationType(ActivatorName, baseName: null)]
+    internal sealed class SmartCalculatorActivator : ISillTextSelectionActivator
+    {
+        internal const string ActivatorName = "SmartCalculator";
+
+        public async ValueTask<bool> GetShouldBeActivatedAsync(string selectedText, bool isReadOnly, CancellationToken cancellationToken)
+        {
+            if (SimpleCalculatorVm.Instance is null)
+                return false;
+
+            SimpleCalculatorVm.Instance.textDocumentAPI.Text = selectedText;
+            var results = await SimpleCalculatorVm.Instance.parserAndInterpreter.WaitAsync();
+
+            if (!SimpleCalculatorVm.Instance.FoundSmartResults(results))
+                return false;
+
+            var result = results![Math.Max(0, results.Count - 2)];
+            bool isError = result.SummarizedResultData!.IsOfType(PredefinedTokenAndDataTypeNames.Error);
+            var output = result.SummarizedResultData.GetDisplayText(Culture.English);
+            if (string.IsNullOrWhiteSpace(output))
+                return false;
+
+            await ThreadHelper.RunOnUIThreadAsync(() =>
+            {
+                SmartCalculatorVm.Instance.CalculatedNumber = output;
+            });
+            return SimpleCalculatorVm.Instance.FoundSmartResults(results);
+        }
+    }
+}
---
+++ src/WindowSill.ScreenRecorder/dev/ViewModels/SmartCalculatorVm.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Recognizers.Text;
+using NotepadBasedCalculator.Core;
+using WindowSill.API;
+using WindowSill.SimpleCalculator.UI;
+
+namespace WindowSill.SimpleCalculator.ViewModels;
+
+public partial class SmartCalculatorVm : ObservableObject
+{
+    public static SmartCalculatorVm Instance { get; private set; }
+
+    [ObservableProperty]
+    private int colorFontSize = 12;
+
+    [ObservableProperty]
+    private int colorboxHeight = 18;
+
+    private string calculatedNumber = "";
+    public string CalculatedNumber
+    {
+        get => calculatedNumber;
+        set
+        {
+            if (calculatedNumber == value)
+                return;
+
+            calculatedNumber = value;
+            OnPropertyChanged(nameof(calculatedNumber));
+        }
+    }
+
+    public TextDocument textDocumentAPI = new TextDocument();
+
+    private const string DefaultCulture = Culture.English;
+
+    private ParserAndInterpreter parserAndInterpreter;
+
+    public SmartCalculatorVm()
+    {
+        Instance = this;
+    }
+
+    public SillView CreateView()
+    {
+        return new SillView { Content = new SmartCalculatorView(this) };
+    }
+}
