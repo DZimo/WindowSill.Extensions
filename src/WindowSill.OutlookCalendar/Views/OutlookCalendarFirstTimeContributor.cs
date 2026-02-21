d2741ca70e3a44d8e0ededb9cd81010a15fecb94
DZimo <53861899+DZimo@users.noreply.github.com>
2026-02-21 19:24:50 +0100

    Outlook calendar - Add of first time views

---
+++ src/WindowSill.OutlookCalendar/Views/OutlookCalendarFirstTimeContributor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using WindowSill.API;
+using WindowSill.OutlookCalendar.Settings;
+
+namespace WindowSill.OutlookCalendar.Views;
+
+public sealed class OutlookCalendarFirstTimeContributor : UserControl, IFirstTimeSetupContributor
+{
+    public bool CanContinue => true;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private SettingsViewModel _settingsViewModel;
+
+    public OutlookCalendarFirstTimeContributor(SettingsViewModel settingsViewModel)
+    {
+        _settingsViewModel = settingsViewModel;
+    }
+
+    public FrameworkElement GetView()
+    {
+        return new OutlookCalendarFirstTimeView(_settingsViewModel);
+    }
+}
---
+++ src/WindowSill.OutlookCalendar/Views/OutlookCalendarFirstTimeView.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.WinUI.Controls;
+using WindowSill.API;
+using WindowSill.OutlookCalendar.Settings;
+using WindowSill.ScreenRecorder.ViewModels;
+
+namespace WindowSill.OutlookCalendar.Views;
+
+public sealed class OutlookCalendarFirstTimeView : UserControl
+{
+    private OutlookCalendarVm _outlookCalendarVm;
+    public OutlookCalendarFirstTimeView(SettingsViewModel settingsViewModel)
+    {
+        this.Content(
+         new StackPanel()
+             .Spacing(16)
+             .Children(
+                 new TextBlock()
+                     .TextWrapping(TextWrapping.WrapWholeWords)
+                     .Text("/WindowSill.OutlookCalendar/Misc/FirstTimeHeadline".GetLocalizedString()),
+
+                 new SettingsCard()
+                     .HeaderIcon(
+                         new FontIcon()
+                             .Glyph("\uE713")
+                     )
+                     .ActionIcon(
+                         new FontIcon()
+                             .Glyph("\uE8A7")
+                     )
+                     .IsClickEnabled(true)
+                     .Content(
+                              new StackPanel()
+                                  .Children(
+                                            new ToggleSwitch()
+                                            .IsOn(x => x.Binding(() => settingsViewModel.SelectedOfficeVersion))
+             )
+     )));
+    }
+
+    public SillListViewButtonItem ExtendAppointments()
+    {
+        return new SillListViewButtonItem('\xE722', new TextBlock().Margin(5).Text("/WindowSill.OutlookCalendar/Misc/DisplayName".GetLocalizedString()), _outlookCalendarVm.Expand);
+    }
+}
